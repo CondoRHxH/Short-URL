8f3563fe8ead2dea5e70829c10cbb133d4b825e8
CondoRHxH <fahdcondor04@gmail.com>
2025-06-26 21:48:53 +0100

    FirstCommit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+function safe_log($message, $filePath = "logs/db_error.log") {
+    $dir = dirname($filePath);
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
+    file_put_contents($filePath, $message, FILE_APPEND);
+}
+
+
+
+
+	$host = "localhost";
+	$username = "root";	
+	$dbName = "url";
+	$dbPassword = "";
+
+	try{
+		$conn = new PDO("mysql:host=$host;dbname=$dbName",$username,$dbPassword);
+		$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		echo "Succefully";
+	}
+	catch(PDOException $e) {
+    safe_log($e->getMessage()."\n");
+		die("Error While Connecting");
+	}
+
+?>
